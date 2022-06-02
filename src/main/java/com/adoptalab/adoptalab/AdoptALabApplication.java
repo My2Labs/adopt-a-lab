b144cd00e9c14cf0d8ad7f09e7d718c702dfbdd5
My2Labs <sking171@ford.com>
2022-06-02 07:06:49 -0500

    Initial commit.

---
+++ src/main/java/com/adoptalab/adoptalab/AdoptALabApplication.java
@@ -0,0 +1,13 @@
+package com.adoptalab.adoptalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptALabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptALabApplication.class, args);
+	}
+
+}
